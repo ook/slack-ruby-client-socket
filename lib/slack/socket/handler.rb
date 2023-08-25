c4a26ab2f774035f3b74cf332943ed94f4fa2eb9
Thomas Lecavelier <thomas@lecavelier.name>
2023-08-26 01:24:50 +0200

    Handler: simplify

--- lib/slack/socket/handler.rb
+++ lib/slack/socket/handler.rb
@@ -10,13 +10,11 @@ module Slack
       end
 
       def on_hello(message)
-        puts "#{DateTime.now.strftime('%Y%m%d-%H:%M:%S')} Connection will restart in "\
+        puts "#{DateTime.now.strftime('%Y%m%d-%H:%M:%S')} Connection will restart in " \
              "#{message.dig('debug_info', 'approximate_connection_time')}s"
       end
 
       def on_disconnect(message)
-        puts 'HANDLER on_disconnect'
-        pp message
         case message['reason']
         when 'warning'
           puts "#{DateTime.now.strftime('%Y%m%d-%H:%M:%S')} Warning: connection disconnection by slack in seconds"
@@ -28,8 +26,7 @@ module Slack
         end
       end
 
-      def on_connection
-      end
+      def on_connection; end
 
       def on_events_api(_message)
         puts 'HANDLER on EVENTS_API'
